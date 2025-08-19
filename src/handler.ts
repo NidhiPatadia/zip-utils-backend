fcc6c8f40dd9a17aeebbbd546c75fdf2850c58c6
Nikunj Mochi <mochinikunj2@gmail.com>
2025-08-19 22:07:53 +0530

    Stopping introspection for PROD

--- src/handler.ts
+++ src/handler.ts
@@ -4,10 +4,14 @@ import {
   startServerAndCreateLambdaHandler,
 } from '@as-integrations/aws-lambda';
 import { resolvers, typeDefs } from './apps/zip-url/graphql';
+import { ENVIRONMENTS } from './configs/common.config';
+
+const NODE_ENV = process.env.NODE_ENV;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  introspection: NODE_ENV !== ENVIRONMENTS.PROD,
 });
 
 export const graphqlHandler = startServerAndCreateLambdaHandler(
