7c2e3729aa4bfd6988667c4a8ac7b962c53f685b
Nidhi Patadia <nidhi.patadia@merkle.com>
2025-08-21 20:49:23 +0530

    Change.

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from '@apollo/server';
 import {
-  handlers,
   startServerAndCreateLambdaHandler,
+  handlers,
 } from '@as-integrations/aws-lambda';
 import { resolvers, typeDefs } from './apps/zip-url/graphql';
 import { ENVIRONMENTS } from './configs/common.config';
@@ -14,7 +14,17 @@ const server = new ApolloServer({
   introspection: NODE_ENV !== ENVIRONMENTS.PROD,
 });
 
-export const graphqlHandler = startServerAndCreateLambdaHandler(
-  server,
-  handlers.createAPIGatewayProxyEventV2RequestHandler(),
-);
+const handler = handlers.createAPIGatewayProxyEventRequestHandler();
+
+export const graphqlHandler = async (event, context) => {
+  const response = await startServerAndCreateLambdaHandler(server, handler)(event, context);
+
+  response.headers = {
+    ...response.headers,
+    'Access-Control-Allow-Origin': 'https://ziputils.com',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    'Access-Control-Allow-Methods': 'OPTIONS, POST',
+  };
+
+  return response;
+};
